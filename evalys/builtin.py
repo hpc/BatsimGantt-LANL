458b82604aaecfca530b45dc1a046eec566f6548
viv-codes <vhafener@outlook.com>
2022-10-03 16:37:21 -0400

    Added evalys folder

--- evalys/builtin.py
+++ evalys/builtin.py
@@ -1,6 +1,6 @@
 from evalys.jobset import JobSet
 import matplotlib.pyplot as plt
 
-js = JobSet.from_csv("data/out_jobs.csv")
+js = JobSet.from_csv("../data/out_jobs.csv")
 js.plot(with_details=True)
 plt.savefig('gantt.png')
