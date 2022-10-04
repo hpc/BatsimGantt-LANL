7b024fff023dd50603aeb00c0725b5ab389f2c09
Viv Hafener <vivi@jet.csh.rit.edu>
2022-10-04 13:51:16 +0000

    Moving files

--- evalys/builtin.py
+++ evalys/builtin.py
@@ -2,5 +2,10 @@ from evalys.jobset import JobSet
 import matplotlib.pyplot as plt
 
 js = JobSet.from_csv("../data/out_jobs.csv")
-js.plot(with_details=True)
+print(js.df.describe())
+
+jf.df.hist()
+
+#js.plot(with_details=True)
+js.gantt()
 plt.savefig('gantt.png')
