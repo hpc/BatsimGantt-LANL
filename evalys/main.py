e917a05e469c80a7cd33fbf491576bf959d95728
viv-codes <vhafener@outlook.com>
2022-10-14 12:13:52 -0400

    Working on parsing the jsons

--- evalys/main.py
+++ evalys/main.py
@@ -1,47 +1,69 @@
 # coding: utf-8
+"""
+Usage:
+    python3 main.py -i <inputpath> [-o <outputfile>] [-r <y/n>]
+
+Required Options:
+    -i <inputpath>    The run directory of the experiment. Eventually this will be the experiment's root directory. Try to use absolute paths, for some reason this doesnt like ~ for homedirs
+
+Optional options:
+    -o <outputfile>    Prompts this program to output the gantt chart to a file instead of showing it to you via the matplotlib viewer. Be careful with this, as it can make large charts unusable
+    -r <y/n>    Prompts this program to look for and utilize reservations when creating the output charts 
+
+"""
+
 import matplotlib.pyplot as plt
 from evalys.jobset import JobSet
 import sys, getopt
 import json
+import os
+
+
+def dictHasKey(myDict, key):
+    if key in myDict.keys():
+        return True
+    else:
+        return False
 
 
 def main(argv):
-    inputfile = ""
+    inputpath = ""
     outputfile = ""
     reservation = ""
     config = ""
+
+    # Parse the arguments passed in
     try:
-        opts, args = getopt.getopt(
-            argv, "hi:o:r:c:", ["ifile=", "ofile=", "resv=", "conf="]
-        )
-        #TODO I can totally parse the locations of both the job file and the input file from the root experiment directory. Do this.
+        opts, args = getopt.getopt(argv, "hi:o:r:c:", ["ipath=", "ofile=", "resv="])
     except getopt.GetoptError:
-        print("main.py -i <inputfile> -o <outputfile> -r <y/n> -c <config.ini>")
+        print("python3 main.py -i <inputpath> [-o <outputfile>] [-r <y/n>]")
         sys.exit(2)
     for opt, arg in opts:
         if opt == "-h":
-            print("main.py -i <inputfile> -o <outputfile> -r <y/n> -c <config.ini>")
+            print("python3 main.py -i <inputpath> [-o <outputfile>] [-r <y/n>]")
             sys.exit(2)
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
+        elif opt in ("-i", "--ipath"):
+            inputpath = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
         elif opt in ("-r", "--resv"):
             reservation = arg
-        elif opt in ("-c", "--conf"):
-            config = arg
 
     # Validate the inputs and confirm that all necessary variables have been provided before proceeding
-    if inputfile == "":
-        print("main.py -i <inputfile> -o <outputfile> -r <y/n> -c <config.ini>")
+    if inputpath == "":
+        print("python3 main.py -i <inputpath> [-o <outputfile>] [-r <y/n>]")
         sys.exit(2)
-    if (reservation == "y" or reservation == "Y") and config == "":
-        print("A value of -c must be provided when using -r!")
-        sys.exit(2)
+
+    # Parse the path of the required files.
+    # TODO Eventually make this able to operate out of the root directory.
+    # TODO This should be try/excepted
+    outJobsCSV = os.path.join(inputpath, "output", "expe-out", "out_jobs.csv")
+    configIn = os.path.join(inputpath, "input", "config.ini")
+    configOut = os.path.join(inputpath, "output", "config.ini")
 
     # If no reservations are specified, process the chart normally
     if reservation == "" or reservation == "n":
-        with open(inputfile) as f:
+        with open(outJobsCSV) as f:
             if sum(1 for line in f) > 20000:
                 print(
                     "Creating gantt charts can be unreliable with files larger than 20k jobs. Are you use you want to continue? (Y/n)"
@@ -51,9 +73,9 @@ def main(argv):
                     pass
                 elif cont == "n":
                     sys.exit(2)
-        js = JobSet.from_csv(inputfile)
+        js = JobSet.from_csv(outJobsCSV)
         print(js.df.describe())
-        # js.df.hist()
+        # js.df.hist() TODO What does this do?
         axe = plt.subplots()
         js.gantt(axe)
         if outputfile == "":
@@ -61,10 +83,22 @@ def main(argv):
         else:
             plt.savefig(outputfile)
     elif reservation == "y" or reservation == "Y":
-        #TODO I should probably run a check here to make sure that config is an .ini
-        OutConfig = json.load(config)
-        totaljs = JobSet.from_csv(inputfile)
-        print("Reservations initialized")
+
+        # Load the config files as a json
+        with open(configIn, "r") as InFile:
+            InConfig = json.load(InFile)
+        with open(configOut, "r") as Infile:
+            OutConfig = json.load(Infile)
+
+        print(
+            str(InConfig["number-of-jobs"])
+            if dictHasKey(InConfig, "synthetic-workload")
+            else False
+        )
+        # Generate a jobset from the input CSV
+
+
+#        totaljs = JobSet.from_csv(inputpath)
 
 
 if __name__ == "__main__":
