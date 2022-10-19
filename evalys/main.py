a9faff5fba636cf6aabcf820b950282f5f961e4b
viv-codes <vhafener@outlook.com>
2022-10-19 15:45:07 -0600

    Made my code safer and added some helpful error messages

--- evalys/main.py
+++ evalys/main.py
@@ -12,20 +12,16 @@ Optional options:
 
 """
 
-# import matplotlib.matplotlib.pyplot as matplotlib.pyplot
-from tkinter import S
 import matplotlib
-matplotlib.use('MacOSX') # Uncomment this line if you're not using macos
+matplotlib.use('MacOSX') # Comment this line if you're not using macos
 from evalys.jobset import JobSet
 from evalys.utils import cut_workload
-from evalys.visu.legacy import plot_gantt_general_shape, plot_gantt # TODO I should probably pull in plot_gantt from the below instead
+from evalys.visu.legacy import plot_gantt # TODO I should probably pull in plot_gantt from the below instead
 from evalys.visu.gantt import plot_gantt_df
 import sys, getopt
 import json
 import os
-import pandas as pd
 from datetime import datetime
-import time
 
 def dictHasKey(myDict, key):
     if key in myDict.keys():
@@ -34,7 +30,7 @@ def dictHasKey(myDict, key):
         return False
 
 
-def makeReservationGantt(row, totaldf, outDir, res_bounds):
+def plotReservationGantt(row, totaldf, outDir, res_bounds):
     reservationStartTime = int(row["starting_time"])
     reservationFinishTime = int(row["finish_time"])
     windowSize = 169200 # TODO Make this not hardcoded. This value is the time in seconds of the windows before and after the reservation.
@@ -44,16 +40,9 @@ def makeReservationGantt(row, totaldf, outDir, res_bounds):
     windowFinishTime = reservationFinishTime + windowSize
     if windowFinishTime>int(totaldf["finish_time"].max()):
         windowFinishTime = int(totaldf["finish_time"].max())
-    # reservationDf = totaldf[(totaldf["starting_time"] >= windowStartTime) & (totaldf["finish_time"] <= windowFinishTime)]
-    cut_js = cut_workload(totaldf,windowStartTime, windowFinishTime)
-    # TODO Extract the reservation bounds from the jobset here and insert
-    plot_gantt_df(cut_js['workload'], res_bounds, title=str("Gantt for reservation starting at time "+str(reservationStartTime)+" and ending at time "+str(reservationFinishTime)+" with "+str(windowSize)+" seconds before and after the reservation"),labels=False)
-    # if outDir == "":
-    #     matplotlib.pyplot.show()
-    # else:
-    #     matplotlib.pyplot.savefig(os.path.join(outDir))
+    cut_js = cut_workload(totaldf,windowStartTime, windowFinishTime) # This is what results in the final crop of the images, it does some cropping of jobs IIRC
+    plot_gantt_df(cut_js['workload'], res_bounds, title=str("Reservation from  "+str(reservationStartTime)+"-"+str(reservationFinishTime)+"+-"+str(windowSize)+"S"))
     matplotlib.pyplot.savefig(os.path.join(outDir, str("reservation")+str(windowStartTime)+"-"+str(windowFinishTime)), dpi=1000)
-    # sys.exit(2) # TODO Remove
 
 def main(argv):
     inputpath = ""
@@ -93,9 +82,9 @@ def main(argv):
     # If no reservations are specified, process the chart normally
     if reservation == "" or reservation == "n":
         with open(outJobsCSV) as f:
-            if sum(1 for line in f) > 50000:
+            if sum(1 for line in f) > 70000:
                 print(
-                    "Creating gantt charts can be unreliable with files larger than 20k jobs. Are you use you want to continue? (Y/n)"
+                    "Creating gantt charts can be unreliable with files larger than 70k jobs. Are you use you want to continue? (Y/n)"
                 )
                 cont = input()
                 if cont == "Y" or cont == "y":
@@ -109,35 +98,31 @@ def main(argv):
             matplotlib.pyplot.show()
         else:
             matplotlib.pyplot.savefig(outputfile)
+
     elif reservation == "y" or reservation == "Y":
-
-        # Load the config files as a json
-        with open(configIn, "r") as InFile:
-            InConfig = json.load(InFile)
-        with open(configOut, "r") as Infile:
-            OutConfig = json.load(Infile)
-
-        # Load reservation information from jsons
-        reservationsResv1 = (
-            InConfig["reservations-resv1"]
-            if dictHasKey(InConfig, "reservations-resv1")
-            else False
-        )
-        reservationsArray = reservationsResv1["reservations-array"]
-
-        # Generate a jobset from the input CSV
-        totaljs = JobSet.from_csv(outJobsCSV)
+        try:
+            with open(configIn, "r") as InFile:
+                InConfig = json.load(InFile)
+            with open(configOut, "r") as Infile:
+                OutConfig = json.load(Infile)
+        except:
+            print("Error! Config files not found! Make sure you're starting from the Run_1 directory.")
+            sys.exit(2)
+        folderNameOut = str(InConfig["batsched-policy"])+"-"+str(InConfig["nodes"])+datetime.now().strftime("%H:%M:%S")
+        if (outputfile == ""):
+            outDir = folderNameOut
+        else:
+            outDir = os.path.join (outputfile, folderNameOut)
+        try:
+            totaljs = JobSet.from_csv(outJobsCSV)
+        except:
+            print("Error! CSV File is empty!")
+            sys.exit(2)
         totaldf = totaljs.df
-
-        # make a directory to dump the output files into
-        # if outputfile != "":
-        outDir = str(InConfig["batsched-policy"])+"-"+str(InConfig["nodes"])+datetime.now().strftime("%H:%M:%S")
         os.mkdir(outDir)
-        # else:
-        #     outDir=""
         for index, row in totaldf.iterrows():
             if (row["purpose"] == "reservation"):
-                makeReservationGantt(row, totaldf, outDir, totaljs.res_bounds)
+                plotReservationGantt(row, totaldf, outDir, totaljs.res_bounds)
 
 
 if __name__ == "__main__":
