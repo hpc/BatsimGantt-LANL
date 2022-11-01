730af28fb5860563666e4dcc19d8c807707e70c6
viv-codes <vhafener@outlook.com>
2022-11-01 07:08:32 -0600

    Added more helpful comments

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ Optional options:
 
 """
 
+from cgitb import reset
+from xml.sax.handler import all_features
 import matplotlib
 
 matplotlib.use("MacOSX")  # Comment this line if you're not using macos
@@ -28,9 +30,15 @@ import json
 import os
 from datetime import datetime
 from yaspin import yaspin
+import pandas as pd
 
 
 def dictHasKey(myDict, key):
+    """
+    Used to check whether key 'key' exists in dict 'mydict'
+
+    :returns: a boolean value representing whether or not the key exists.
+    """
     if key in myDict.keys():
         return True
     else:
@@ -38,6 +46,10 @@ def dictHasKey(myDict, key):
 
 
 def plotReservationGantt(row, totaldf, outDir, res_bounds, verbosity, maxJobLen):
+    """
+    Plots a standard gantt reservation, without binning by job size.
+    This produces an output folder containing one image for each reservation.
+    """
     reservationStartTime = int(row["starting_time"])
     reservationFinishTime = int(row["finish_time"])
     reservationExecTime = int(row["execution_time"])
@@ -65,6 +77,7 @@ def plotReservationGantt(row, totaldf, outDir, res_bounds, verbosity, maxJobLen)
     )
     if verbosity == True:
         print(cut_js)
+    # FIXME This could use saveDfPlot
     plot_gantt_df(
         cut_js["workload"],
         res_bounds,
@@ -101,6 +114,9 @@ def plotReservationGantt(row, totaldf, outDir, res_bounds, verbosity, maxJobLen)
 
 
 def plotBinnedGanttReservations(row, totaldf, outDir, res_bounds, verbosity, maxJobLen):
+    """
+    Plots gantt charts for reservations, binning into 3 different job types based on runtime and requested resources.
+    """
     reservationStartTime = int(row["starting_time"])
     reservationFinishTime = int(row["finish_time"])
     reservationExecTime = int(row["execution_time"])
@@ -128,28 +144,38 @@ def plotBinnedGanttReservations(row, totaldf, outDir, res_bounds, verbosity, max
     )
     if verbosity == True:
         print(cut_js)
-
-    smallJs, longJs, largeJs = binDfToJs(cut_js["workload"])
-    outPath = os.path.join(
-        outDir, str("BIN-" + str(windowStartTime) + str(windowFinishTime))
-    )
-    os.mkdir(outPath)
-    # saveTriplePlot(cut_js, outPath, reservationStartTime,reservationExecTime, reservationInterval)
-    saveDfPlot(
-        smallJs,
-        getFileName("smallJs", outPath),
-        reservationStartTime,
-        reservationExecTime,
-        reservationInterval,
-        longJs=longJs,
-        largeJs=largeJs,
-        windowStartTime=windowStartTime,
-        windowFinishtime=windowFinishTime,
-        binned=True,
-    )
+    try:
+        smallJs, longJs, largeJs = binDfToJs(cut_js["workload"])
+        saveDfPlot(
+            smallJs,
+            getFileName(
+                str("BIN-" + str(windowStartTime) + "-" + str(windowFinishTime)), outDir
+            ),
+            reservationStartTime,
+            reservationExecTime,
+            reservationInterval,
+            longJs=longJs,
+            largeJs=largeJs,
+            windowStartTime=windowStartTime,
+            windowFinishtime=windowFinishTime,
+            binned=True,
+        )
+    except ValueError:
+        print(
+            "WARNING: Your dataset contains reservations that are not surrounded by any jobs. Skipping reservation from "
+            + str(reservationStartTime)
+            + "-"
+            + str(reservationFinishTime)
+        )
 
 
 def binDfToJs(df):
+    """
+    Used to sort jobs within a DF into 3 types based on runtime and requested resources,
+    then convert those dfs into jobsets.
+
+    :returns: 3 jobsets, sorted by the resource parameters
+    """
     smallDf = df.loc[
         (df["requested_number_of_resources"] <= 32) & (df["execution_time"] <= 28800)
     ]
@@ -161,6 +187,20 @@ def binDfToJs(df):
     largeDf = df.loc[(df["requested_number_of_resources"] > 32)]
     largeJs = JobSet.from_df(largeDf)
     return smallJs, longJs, largeJs
+
+
+def binDf(df):
+    """
+    Used to sort jobs within a DF into 3 types based on runtime and requested resources and output those dataframes.
+    """
+    smallDf = df.loc[
+        (df["requested_number_of_resources"] <= 32) & (df["execution_time"] <= 28800)
+    ]
+    longDf = df.loc[
+        (df["requested_number_of_resources"] <= 32) & (df["execution_time"] > 28800)
+    ]
+    largeDf = df.loc[(df["requested_number_of_resources"] > 32)]
+    return smallDf, longDf, largeDf
 
 
 def saveDfPlot(
@@ -175,6 +215,9 @@ def saveDfPlot(
     windowFinishtime=None,
     binned=False,
 ):
+    """
+    Saves a plot from a jobset based on the given parameters.
+    """
     js.plot(
         with_gantt=True,
         longJs=longJs,
@@ -191,13 +234,24 @@ def saveDfPlot(
         dpi=300,
     )
     matplotlib.pyplot.close()
+    print("\nSaved figure to: " + outfile)
 
 
 def getFileName(name, outPath):
+    """
+    Returns a filename to be used based on the provided name and output path
+
+    :returns: the output path as a string
+    """
     return os.path.join(outPath, name)
 
 
 def loadConfigs(inputpath):
+    """
+    Parses the provided configuration files as JSON and loads them
+
+    :returns: the two config files loaded into JSON objects
+    """
     configIn = os.path.join(inputpath, "input", "config.ini")
     configOut = os.path.join(inputpath, "output", "config.ini")
     with yaspin().line as sp:
@@ -216,6 +270,11 @@ def loadConfigs(inputpath):
 
 
 def getOutputDir(InConfig, outputfile):
+    """
+    Parses the output folder or file from the config file
+
+    :returns: a path to be used as the output path, or a path for the output file
+    """
     with yaspin().line as sp:
         sp.text = "Determining output folder"
         folderNameOut = (
@@ -233,6 +292,11 @@ def getOutputDir(InConfig, outputfile):
 
 
 def dfFromCsv(outJobsCSV):
+    """
+    Generates a dataframe and jobset from the provided CSV file
+
+    :returns: a dataframe from the jobset, and the jobset itself
+    """
     with yaspin().line as sp:
         sp.text = "Creating jobset from out_jobs.csv"
         try:
@@ -244,12 +308,20 @@ def dfFromCsv(outJobsCSV):
 
 
 def getMaxJobLen(totaldf):
+    """
+    Gets the length of the longest job in a dataframe
+
+    :returns: the length of the longest job in the df
+    """
     maxJobLen = totaldf["execution_time"].max()
     print("Maximum job length parsed as: " + str(maxJobLen))
     return maxJobLen
 
 
 def iterateReservations(inputpath, outputfile, outJobsCSV, verbosity, binned=False):
+    """
+    Iterates over reservations and plots them based on whether or not they're binned
+    """
     InConfig, OutConfig = loadConfigs(inputpath)
     outDir = getOutputDir(InConfig, outputfile)
     totaldf, totaljs = dfFromCsv(outJobsCSV)
@@ -287,33 +359,147 @@ def iterateReservations(inputpath, outputfile, outJobsCSV, verbosity, binned=Fal
                 #     print(e)
 
 
+def chartRunningAverage(
+    inputpath,
+    outputfile,
+    outJobsCSV,
+):
+    """
+    Charts the running average of utilization
+    """
+    InConfig, OutConfig = loadConfigs(inputpath)
+    outDir = getOutputDir(InConfig, outputfile)
+    totaldf, totaljs = dfFromCsv(outJobsCSV)
+    maxJobLen = getMaxJobLen(totaldf)
+
+    smallDf = []
+    longDf = []
+    largeDf = []
+    allResvDf = [smallDf, longDf, largeDf]
+
+    for index, row in totaldf.iterrows():
+        if row["purpose"] == "reservation":
+            with yaspin().line as sp:
+                sp.text = (
+                    "Plotting gantt chart for reservation from:"
+                    + str(row["starting_time"])
+                    + " to "
+                    + str(row["finish_time"])
+                )
+                #! Catch empty DFs here
+                allresvDf = prepDf(row, totaldf, maxJobLen, allResvDf)
+                print(
+                    "Reservation from: "
+                    + str(row["starting_time"])
+                    + "-"
+                    + str(row["finish_time"])
+                    + " added to df."
+                )
+    smallJs = JobSet.from_df(allResvDf[0])
+    longJs = JobSet.from_df(allResvDf[1])
+    largeJs = JobSet.from_df(allResvDf[2])
+    # TODO Unhardcode the window start and finish times
+    # ! THIS IS WRONG AND DOES NOT PLOT AVERAGE RIGHT NOW. THIS IS ONLY AN OUTLINE
+    smallJs.plot(
+        with_gantt=False,
+        longJs=longJs,
+        largeJs=largeJs,
+        average=True,
+    )
+    outfile = getFileName(str("testing"), outDir)
+    matplotlib.pyplot.savefig(
+        outfile,
+        dpi=300,
+    )
+    matplotlib.pyplot.close()
+    print("\nSaved figure to: " + outfile)
+
+
+def prepDf(row, totaldf, maxJobLen, allResvDf):
+    """
+    Using the given row, parses the jobs in a window, bins those jobs based on size,
+    and concatenates them with the dataframes stored in allResvDf.
+
+    :returns: a list of the three types of jobs, containing 3 dataframes, one for each size,
+    with the details from this row appended.
+    """
+    # Defining reservation and window bounds
+    reservationStartTime = int(row["starting_time"])
+    reservationFinishTime = int(row["finish_time"])
+    reservationExecTime = int(row["execution_time"])
+    reservationInterval = row["allocated_resources"]
+    windowSize = 169200
+    windowStartTime = reservationStartTime - windowSize
+    if windowStartTime < 0:
+        windowStartTime = 0
+    windowFinishTime = reservationFinishTime + windowSize
+    if windowFinishTime > int(totaldf["finish_time"].max()):
+        windowFinishTime = int(totaldf["finish_time"].max())
+
+    cut_js = cut_workload(
+        totaldf, windowStartTime - maxJobLen, windowFinishTime + maxJobLen
+    )
+    cut_js = resetDfTimescale(cut_js, windowStartTime)
+    smallDf, longDf, largeDf = binDf(cut_js["workload"])
+    # smallDf = resetDfTimescale(smallDf)
+    # longDf = resetDfTimescale(longDf)
+    # largeDf = resetDfTimescale(largeDf)
+    # print(allResvDf[0])
+    if len(allResvDf[0]) == 0:
+        allResvDf[0] = smallDf
+        allResvDf[1] = longDf
+        allResvDf[2] = largeDf
+    else:
+        allResvDf[0] = pd.concat([allResvDf[0], smallDf])
+        allResvDf[1] = pd.concat([allResvDf[1], longDf])
+        allResvDf[2] = pd.concat([allResvDf[2], longDf])
+
+    return allResvDf
+
+
+def resetDfTimescale(df, windowStartTime):
+    """
+    Iterates over records in a df, and sets their starting and finish times based on a zero of the windowStartTime
+
+    :returns: The df with time data reset
+    """
+    for index, row in df["workload"].iterrows():
+        row["starting_time"] = row["starting_time"] - windowStartTime
+        row["finish_time"] = row["finish_time"] - windowStartTime
+        print(row)
+    return df
+
+
 def main(argv):
     inputpath = ""
     outputfile = ""
     reservation = False
     verbosity = False
     binned = False
+    average = False
 
     # Parse the arguments passed in
     try:
         opts, args = getopt.getopt(
-            argv, "hi:o:r:v:b:", ["ipath=", "ofile=", "resv=", "verbosity=", "binned="]
+            argv,
+            "hi:o:r:v:b:a:",
+            ["ipath=", "ofile=", "resv=", "verbosity=", "binned=", "average="],
         )
     except getopt.GetoptError:
         print(
-            "python3 main.py -i <inputpath> [-o <outputfile>] [-r <y/N>] [-v <y/N>] [-b <y/N]"
+            "Option error! See usage below:\npython3 main.py -i <inputpath> [-o <outputfile>] [-r <y/N>] [-v <y/N>] [-b <y/N>] [-a <y/N>]"
         )
         sys.exit(2)
     for opt, arg in opts:
         if opt == "-h":
             print(
-                "python3 main.py -i <inputpath> [-o <outputfile>] [-r <y/N>] [-v <y/N>] [-b <y/N]"
+                "Help menu:\npython3 main.py -i <inputpath> [-o <outputfile>] [-r <y/N>] [-v <y/N>] [-b <y/N] [-a <y/N>]"
             )
             sys.exit(2)
         elif opt in ("-i", "--ipath"):
             if arg == "":
                 print(
-                    "python3 main.py -i <inputpath> [-o <outputfile>] [-r <y/N>] [-v <y/N>] [-b <y/N]"
+                    "python3 main.py -i <inputpath> [-o <outputfile>] [-r <y/N>] [-v <y/N>] [-b <y/N] [-a <y/N>]\n Please supply an input path!"
                 )
                 sys.exit(2)
             else:
@@ -329,11 +515,11 @@ def main(argv):
         elif opt in ("-b", "--binned"):
             if arg.lower() == "y":
                 binned = True
+        elif opt in ("-a", "--average"):
+            if arg.lower() == "y":
+                average = True
 
     # Parse the path of the required files.
-    # TODO Eventually make this able to operate out of the root directory.
-    # TODO This should be try/excepted
-    # TODO I should also make an option so that this can just go to files in the local directory
     outJobsCSV = os.path.join(inputpath, "output", "expe-out", "out_jobs.csv")
 
     # If no reservations are specified, process the chart normally
@@ -356,8 +542,11 @@ def main(argv):
         else:
             matplotlib.pyplot.savefig(outputfile)
 
-    elif reservation:
+    elif reservation and not average:
         iterateReservations(inputpath, outputfile, outJobsCSV, verbosity, binned)
+
+    elif reservation and average:
+        chartRunningAverage(inputpath, outputfile, outJobsCSV)
 
     else:
         print("Incompatible options entered! Please try again")
